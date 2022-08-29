d50d935dc087d4d9d877271c69a5478cd01a7bb5
Anthony Chao <anthonyschaowder@gmail.com>
2022-08-29 09:51:03 -0400

    complete frontend user auth

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/api/users", users);
-// app.use("/api/tweets", tweets);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Branches</h1>
+        
+      </div>
+    );
+  }
+}
+
+export default MainPage;
