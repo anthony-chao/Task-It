41ff6c3bf0608c1683cc3627d87d05cd43e09d63
MichaelNgCen <michaelngcen@yahoo.com>
2022-08-29 19:48:48 -0400

    Worked On Splash React & CSS

---
+++ models/Task 2.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const TaskSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    assignedUser: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    projectId : {
+        type: Schema.Types.ObjectId,
+        ref: 'project',
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["Completed", "In Progress", "Invalid"],
+        required: true
+    },
+    subtasks: [this]
+})
+
+module.exports = Task = mongoose.model('tasks', TaskSchema)
