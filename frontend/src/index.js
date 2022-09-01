a02e1736c9725a1bdd12e2247b3e5f6c081f8f8a
pdeguzman <padgzmn@gmail.com>
2022-09-01 12:03:38 -0400

    add styling on chat

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,12 @@ import configureStore from "./store/store";
 import jwt_decode from "jwt-decode";
 import { setAuthToken } from "./util/sessionApiUtil";
 import { login, logout } from "./actions/sessionActions";
-import { fetchProjects, fetchProject, fetchUserProjects, deleteProject } from "./actions/projectActions";
+import {
+  fetchProjects,
+  fetchProject,
+  fetchUserProjects,
+  deleteProject,
+} from "./actions/projectActions";
 
 import "./assets/stylesheets/main.scss";
 
@@ -25,7 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (decodedUser.exp < currentTime) {
       store.dispatch(logout());
-      window.location.href = "/login";
+      window.location.href = "/";
     }
   } else {
     store = configureStore({});
