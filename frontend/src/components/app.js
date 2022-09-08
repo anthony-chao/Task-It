623a2825a82073773b566a829afd923e821f452e
pdeguzman <padgzmn@gmail.com>
2022-09-08 14:23:05 -0400

    replace dashboard to be home page and create about us section, and replace drawer with permanent sidedrawer

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,12 +3,14 @@ import { AuthRoute, ProtectedRoute } from "../util/routeUtil";
 import { Switch, Route } from "react-router-dom";
 import NavBarContainer from "./navbar/navbar";
 import SplashContainer from "./splash/splash";
-import DashboardContainer from "./dashboard/Dashboard";
+import HomeContainer from "./home/Home";
+import Chat from "./chat/chat";
 
 const App = () => (
   <div className="main-container">
     <NavBarContainer />
-    <ProtectedRoute path="/dashboard" component={DashboardContainer} />
+    <ProtectedRoute path="/home" component={HomeContainer} />
+    <Chat />
     <Switch>
       <AuthRoute exact path="/" component={SplashContainer} />
     </Switch>
